06711cef3c718272913ba57fa36624c2a820df9e
michael qian <michaelcqian@163.com>
2018-05-08 13:31:26 +0800

    Add file requirement\common.txt

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,7 @@ from flask import render_template, make_response, send_file, url_for, request, c
 from ..models import Carpark, Transaction
 from datetime import datetime
 
+
 @main.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
@@ -12,11 +13,13 @@ def index():
     carparks = pagination.items
     return render_template('index.html', carparks=carparks, pagination=pagination)
 
+
 @main.route('/carpark/<code>')
 def carpark(code):
     carpark = Carpark.query.filter_by(code=code).first_or_404()
     volume_chart = carpark.gen_volume_chart()
     return render_template('carpark.html', carpark=carpark, volume_chart=volume_chart, filepath=url_for('static',filename='images/foo.png'))
+
 
 @main.route('/transactions/<code>')
 def transactions(code):
